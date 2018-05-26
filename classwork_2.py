c8ec36e10b9e09182c3011dda7f130b400f1cfa1
kjokkjok <36381685+kjokkjok@users.noreply.github.com>
2018-05-27 02:39:06 +0300

    Update classwork_2.py

--- classwork_2.py
+++ classwork_2.py
@@ -7,17 +7,26 @@
 from Bio.Blast import NCBIWWW
 from Bio.Blast import NCBIXML
 from Bio import SeqIO
-with open("blast_result.fasta", "w") as out_handle:
 
-        for record in SeqIO.parse("./classwork2.fasta", format="fasta"):
+import argparse
 
-            result_handle = NCBIWWW.qblast("blastn", "nt", record.seq,hitlist_size=1, descriptions=1,alignments=1)
+parser = argparse.ArgumentParser(description='script does query to blast database')
+parser.add_argument( '-in', '--input_file', help = 'path to file in fasta format', type = str, metavar = 'str')
+parser.add_argument( '-out', '--output_file', help = 'path to file for output', type = str, metavar = 'str')
 
-            blast_records = NCBIXML.parse(result_handle)
-        
-            for blast_record in blast_records:
-                for alignment in blast_record.alignments:
-                    for hsp in alignment.hsps:
-                        out_handle.write(">"+alignment.hit_def+"\n"+hsp.query+"\n")
-                        break
+args = parser.parse_args()
+
+
+with open(args.output_file, "w") as out_handle:
+    for record in SeqIO.parse("args.input_file", format="fasta"):
+
+        result_handle = NCBIWWW.qblast("blastn", "nt", record.seq,hitlist_size=1, descriptions=1,alignments=1)
+
+        blast_records = NCBIXML.parse(result_handle)
+
+        for blast_record in blast_records:
+            for alignment in blast_record.alignments:
+                for hsp in alignment.hsps:
+                    out_handle.write(">"+alignment.hit_def+"\n"+hsp.query+"\n")
+                    break
 
